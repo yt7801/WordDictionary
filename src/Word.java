e3ba2da13602aa791ad39885ee7ed691a683ac2d
ykane <ykane@192.168.1.20>
2025-06-29 11:00:40 +0900

    コミット

--- src/Word.java
+++ src/Word.java
@@ -1,9 +1,28 @@
 
 public class Word {
-
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		
+	private String term; //単語
+	private String meaning; //意味
+	
+	//初期化
+	public Word(String term, String meaning) {
+		this.term = term;
+		this.meaning = meaning;
+	}
+	
+	//単語を取得
+	public String getTerm() {
+		return term;
+	}
+	
+	//意味を取得
+	public String getMeaning() {
+		return meaning;
+	}
+	
+	//単語と意味を文字列として返す
+	@Override
+	public String toString() {
+		return term + ": " + meaning;
 	}
 
 }
